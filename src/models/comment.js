2189954f608747c2a5fe54d908155efdce1428b4
shoryan5135s <shoryanagarwal154@gmail.com>
2026-03-31 15:29:26 +0530

    updated model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,6 +6,32 @@ const commentSchema =new mongoose.Schema({
         required:true
     },
 
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+
+
+    onModel:{
+
+    type: String,
+    required:true,
+    enum:['Tweet','Comment']
+
+
+
+
+   },
+
+   commentable:{
+        type:mongoose.Schema.Types.ObjectId,
+        refPath:'onModel',
+        required:true
+        
+   }
+
+
    
 
 },{timestamps:true})
